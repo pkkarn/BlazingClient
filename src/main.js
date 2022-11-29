4066bec96c57910f459f5de2a56365a6e4cdaafa
Prashant Kumar <pk2psp@gmail.com>
2022-11-29 14:56:51 +0530

    Socket address changed

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 
 import "./assets/main.css";
 
-const socket = io.connect("http://localhost:4200"); // Establish the connection
+const socket = io.connect("https://socket-codechit.herokuapp.com"); // Establish the connection
 
 const app = createApp(App);
 
