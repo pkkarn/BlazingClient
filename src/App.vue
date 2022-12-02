d38fea7552a65008e30e11cf44f83feac968638d
Prashant Kumar <pk2psp@gmail.com>
2022-12-02 23:48:44 +0530

    Layout and rooms added

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
-<script setup>
-import BlazeView from "./views/BlazeView.vue";
-</script>
 <template>
-  <div>
-    <BlazeView />
+  <div id="app">
+    <h1>Blazing8</h1>
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "./assets/sass/variables";
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;900&display=swap");
+
+#app {
+  min-height: 100vh;
+  color: $font-color;
+  margin: 0 auto;
+  max-width: 80%;
+  font-family: $font-family;
+  font-weight: 400;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Blaze from "@/views/BlazeView.vue";
+import Home from "@/views/HomeView.vue";
+import Blaze from "@/components/BlazingDashboard.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "blaze",
-      component: Blaze,
+      name: "home",
+      component: Home,
     },
     {
-      path: "/about",
-      name: "about",
+      path: "/blaze",
+      name: "blaze",
+      props: true,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      component: () => import("../components/BlazingDashboard.vue"),
     },
   ],
 });
